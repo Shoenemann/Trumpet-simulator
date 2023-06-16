82ec4c0fe9334a008167ab39f58e175904b296d3
Shoenemann <85433055+Shoenemann@users.noreply.github.com>
2023-06-17 01:31:51 +0200

    aggiunti gli onclicklisteners app compila!

--- app/src/main/java/com/example/trumpetsimulator/MainActivity.java
+++ app/src/main/java/com/example/trumpetsimulator/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     boolean [] pistoni_premuti = {false,false,false};
     TextView debugText;
+    int a;
+
     Button pistone1,pistone2,pistone3;
 
 
@@ -19,11 +21,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        debugText = (TextView) findViewById(R.id.debugText);
-        //debugText.setText("ciao");
+        debugText = findViewById(R.id.debugText);
+        debugText.setText("ciao");
 
-        pistone1 = (Button) findViewById(R.id.pistone1);
-        pistone2 = (Button) findViewById(R.id.pistone2);
-        pistone3 = (Button) findViewById(R.id.pistone3);
+
+        pistone1 = findViewById(R.id.pistone1);
+        pistone2 = findViewById(R.id.pistone2);
+        pistone3 = findViewById(R.id.pistone3);
+
+        pistone1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                piston_press(1);
+            }
+        });
+        pistone2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                piston_press(2);
+            }
+        });
+        pistone3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                piston_press(3);
+            }
+        });
+    }
+
+    public void piston_effect() {
+        String debugPistoni = "";
+        for (int i = 0; i<3; i++) {
+            if (pistoni_premuti[i]){
+                debugPistoni += "pistone " +String.valueOf(i+1) ;
+            }
+        }
+        debugText.setText(debugPistoni);
+    }
+
+    public void piston_press(int qualePistone){
+        // I pistoni di una tromba sono 1,2,3 ma le liste in Java iniziano da 0
+        pistoni_premuti[qualePistone-1] = true;
+        piston_effect();
     }
 }
